957077e6316e84e2203c1792628a42ef6be08805
Rustam Zagirov <stammru@gmail.com>
2019-06-09 09:47:40 +0200

    fix source for docker-ce

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -29,7 +29,7 @@ class kubernetes::repos (
           }
         }
         'docker-ce': {
-          apt::source { $docker_package_name:
+          apt::source { 'docker-ce':
             architecture  => 'amd64',
             location      => 'https://download.docker.com/linux/ubuntu',
             repos         => 'stable',
